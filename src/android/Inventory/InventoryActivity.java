39d9f265315c9dc04ae1ce3a481a9235acc28a93
unknown <adamf2400@gmail.com>
2019-12-13 03:55:13 +0800

    fix: some bugs

--- src/android/Inventory/InventoryActivity.java
+++ src/android/Inventory/InventoryActivity.java
@@ -29,7 +29,7 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
-import android.support.v4.content.LocalBroadcastManager;
+import androidx.localbroadcastmanager.content.LocalBroadcastManager;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.Menu;
