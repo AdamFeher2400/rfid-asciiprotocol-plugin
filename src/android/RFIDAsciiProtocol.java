581618dfcb319e1a292ff53c16387a6c3a725e3f
unknown <adamf2400@gmail.com>
2019-12-27 22:54:01 +0800

    fix: some issues

--- src/android/RFIDAsciiProtocol.java
+++ src/android/RFIDAsciiProtocol.java
@@ -31,35 +31,67 @@ import com.uk.tsl.utils.Observable;
 
 public class RFIDAsciiProtocol extends CordovaPlugin {
     private static final String TAG = "RFIDAsciiProtocol";
+    
+	// All of the reader inventory tasks are handled by this class
+	private InventoryModel mModel;
 
+    // The Reader currently in use
     private Reader mReader = null;
+
+    public RFIDAsciiProtocol() {
+    }
+
+    private void init() {
+        mGenericModelHandler = new GenericHandler(this);
+
+        Activity activity = cordova.getActivity();
+        
+		// Ensure the shared instance of AsciiCommander exists
+        AsciiCommander.createSharedInstance(activity.getApplicationContext());
+    	AsciiCommander commander = getCommander();
+
+        // Ensure that all existing responders are removed
+        commander.clearResponders();
+
+		// Add the LoggerResponder - this simply echoes all lines received from the reader to the log
+        // and passes the line onto the next responder
+        // This is added first so that no other responder can consume received lines before they are logged.
+        commander.addResponder(new LoggerResponder());
+
+        // Add a synchronous responder to handle synchronous commands
+        commander.addSynchronousResponder();
+
+        // Create the single shared instance for this ApplicationContext
+        ReaderManager.create(activity.getApplicationContext());
+
+        // Add observers for changes
+        ReaderManager.sharedInstance().getReaderList().readerRemovedEvent().addObserver(mRemovedObserver);
+
+        //Create a (custom) model and configure its commander and handler
+        mModel = new InventoryModel();
+        mModel.setCommander(getCommander());
+        mModel.setHandler(mGenericModelHandler);
+        mModel.setEnabled(true);
+
+        // Register to receive notifications from the AsciiCommander
+        LocalBroadcastManager.getInstance(activity).registerReceiver(mCommanderMessageReceiver, new IntentFilter(AsciiCommander.STATE_CHANGED_NOTIFICATION));
+        // The ReaderManager needs to know about Activity lifecycle changes
+        ReaderManager.sharedInstance().onResume();
+
+        // The Activity may start with a reader already connected (perhaps by another App)
+        // Update the ReaderList which will add any unknown reader, firing events appropriately
+        ReaderManager.sharedInstance().updateList();
+    }
+
     //----------------------------------------------------------------------------------------------
     // ReaderList Observers
     //----------------------------------------------------------------------------------------------
-    Observable.Observer<Reader> mAddedObserver = new Observable.Observer<Reader>()
-    {
-        @Override
-        public void update(Observable<? extends Reader> observable, Reader reader)
-        {
-            // See if this newly added Reader should be used
-            AutoSelectReader(true);
-        }
-    };
-
-    Observable.Observer<Reader> mUpdatedObserver = new Observable.Observer<Reader>()
-    {
-        @Override
-        public void update(Observable<? extends Reader> observable, Reader reader)
-        {
-        }
-    };
 
     Observable.Observer<Reader> mRemovedObserver = new Observable.Observer<Reader>()
     {
         @Override
         public void update(Observable<? extends Reader> observable, Reader reader)
         {
-            mReader = null;
             // Was the current Reader removed
             if( reader == mReader)
             {
@@ -71,70 +103,48 @@ public class RFIDAsciiProtocol extends CordovaPlugin {
         }
     };
 
-    private void AutoSelectReader(boolean attemptReconnect)
+    //----------------------------------------------------------------------------------------------
+	// Model notifications
+	//----------------------------------------------------------------------------------------------
+
+    private static class GenericHandler extends WeakHandler<RFIDAsciiProtocol>
     {
-        ObservableReaderList readerList = ReaderManager.sharedInstance().getReaderList();
-        Reader usbReader = null;
-        if( readerList.list().size() >= 1)
+        public GenericHandler(RFIDAsciiProtocol t)
         {
-            // Currently only support a single USB connected device so we can safely take the
-            // first CONNECTED reader if there is one
-            for (Reader reader : readerList.list())
-            {
-                IAsciiTransport transport = reader.getActiveTransport();
-                if (reader.hasTransportOfType(TransportType.USB))
-                {
-                    usbReader = reader;
-                    break;
-                }
-            }
+            super(t);
         }
 
-        if( mReader == null )
+        @Override
+        public void handleMessage(Message msg, RFIDAsciiProtocol t)
         {
-            if( usbReader != null )
-            {
-                // Use the Reader found, if any
-                mReader = usbReader;
-                getCommander().setReader(mReader);
-            }
-        }
-        else
-        {
-            // If already connected to a Reader by anything other than USB then
-            // switch to the USB Reader
-            IAsciiTransport activeTransport = mReader.getActiveTransport();
-            if ( activeTransport != null && activeTransport.type() != TransportType.USB && usbReader != null)
-            {
-                mReader.disconnect();
-
-                mReader = usbReader;
-
-                // Use the Reader found, if any
-                getCommander().setReader(mReader);
-            }
-        }
-
-        // Reconnect to the chosen Reader
-        if( mReader != null && (mReader.getActiveTransport()== null || mReader.getActiveTransport().connectionStatus().value() == ConnectionState.DISCONNECTED))
-        {
-            // Attempt to reconnect on the last used transport unless the ReaderManager is cause of OnPause (USB device connecting)
-            if( attemptReconnect )
-            {
-                if( mReader.allowMultipleTransports() || mReader.getLastTransportType() == null )
-                {
-                    // Reader allows multiple transports or has not yet been connected so connect to it over any available transport
-                    mReader.connect();
-                }
-                else
-                {
-                    // Reader supports only a single active transport so connect to it over the transport that was last in use
-                    mReader.connect(mReader.getLastTransportType());
-                }
-            }
-        }
-    }
+			try {
+				switch (msg.what) {
+				case ModelBase.MESSAGE_NOTIFICATION:
+					// Examine the message for prefix
+					String message = (String)msg.obj;
+					if( message.startsWith("BC:")) {
+                        // Barcode Handler
+                        final PluginResult result = new PluginResult(PluginResult.Status.OK, message);
+                        t.callback.sendPluginResult(result);
+					}
+					break;
+					
+				default:
+					break;
+				}
+			} catch (Exception e) {
+			}
+			
+		}
+    };
     
+    // The handler for model messages
+    private static GenericHandler mGenericModelHandler;
+
+    //----------------------------------------------------------------------------------------------
+	// AsciiCommander message handling
+	//----------------------------------------------------------------------------------------------
+
     /**
      * @return the current AsciiCommander
      */
@@ -143,64 +153,120 @@ public class RFIDAsciiProtocol extends CordovaPlugin {
         return AsciiCommander.sharedInstance();
     }
 
-    public RFIDAsciiProtocol() {
-    }
+    //
+    // Handle the messages broadcast from the AsciiCommander
+    //
+    private BroadcastReceiver mCommanderMessageReceiver = new BroadcastReceiver() {
+    	@Override
+    	public void onReceive(Context context, Intent intent) {
+            if( getCommander().isConnected() )
+            {    			
+            	mModel.resetDevice();
+                mModel.updateConfiguration();
+            }
+    	}
+    };
+
+
 
     public void initialize(CordovaInterface cordova, CordovaWebView webView) {
         super.initialize(cordova, webView);
-
-        // AsciiCommander.createSharedInstance(cordova.getActivity().getApplicationContext());
-
-    	// AsciiCommander commander = getCommander();
-
-        // // Ensure that all existing responders are removed
-        // commander.clearResponders();
-
-		// // Add the LoggerResponder - this simply echoes all lines received from the reader to the log
-        // // and passes the line onto the next responder
-        // // This is added first so that no other responder can consume received lines before they are logged.
-        // commander.addResponder(new LoggerResponder());
-
-        // // Add a synchronous responder to handle synchronous commands
-        // commander.addSynchronousResponder();
-
-        // // Create the single shared instance for this ApplicationContext
-        // ReaderManager.create(cordova.getActivity().getApplicationContext());
-
-        // // Add observers for changes
-        // ReaderManager.sharedInstance().getReaderList().readerAddedEvent().addObserver(mAddedObserver);
-        // ReaderManager.sharedInstance().getReaderList().readerUpdatedEvent().addObserver(mUpdatedObserver);
-        // ReaderManager.sharedInstance().getReaderList().readerRemovedEvent().addObserver(mRemovedObserver);
     }
 
+    public CallbackContext callback = null;
     public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
-        if ("isConnected".equals(action)) {
+        if("init".equals(action)) {
+            init();
+        }
+        else if ("isConnected".equals(action)) {
             final PluginResult result = new PluginResult(PluginResult.Status.OK, String.valueOf(isConnected()));
             callbackContext.sendPluginResult(result);
         }
         else if ("connect".equals(action)) {
+            callback = callbackContext;
             connect();
-            final PluginResult result = new PluginResult(PluginResult.Status.OK, "success");
-            callbackContext.sendPluginResult(result);
+        }
+        else if("disconnect".equals(action)) {
+            disconnect();
         }
         else if("scan".equals(action)) {
-            final PluginResult result = new PluginResult(PluginResult.Status.OK, scan());
-            callbackContext.sendPluginResult(result);
+            scan();
         }
         return true;
     }
 
     private boolean isConnected() {
-        return false;
+        return mReader != null && mReader.isConnected();
     }
 
     private void connect() {
+        cordova.setActivityResultCallback(this);
         Context context = cordova.getActivity().getApplicationContext();
-        Intent intent = new Intent(context, InventoryActivity.class);
-        this.cordova.getActivity().startActivity(intent);
+        Intent selectIntent = new Intent(context, DeviceListActivity.class);
+        cordova.startActivityForResult(this, selectIntent, DeviceListActivity.SELECT_DEVICE_REQUEST);
     }
 
-    private String scan() {
-        return "BC: 22222";
+    private void disconnect() {
+        if( mReader != null )
+        {
+            mReader.disconnect();
+            mReader = null;
+        }
+    }
+
+    private void scan() {
+        try {
+            // Perform a transponder scan
+            mModel.scan();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    //
+    // Handle Intent results
+    //
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data)
+    {
+        switch (requestCode)
+        {
+            case DeviceListActivity.SELECT_DEVICE_REQUEST:
+                // When DeviceListActivity returns with a device to connect
+                if (resultCode == Activity.RESULT_OK)
+                {
+                    int readerIndex = data.getExtras().getInt(EXTRA_DEVICE_INDEX);
+                    Reader chosenReader = ReaderManager.sharedInstance().getReaderList().list().get(readerIndex);
+
+                    int action = data.getExtras().getInt(EXTRA_DEVICE_ACTION);
+
+                    // If already connected to a different reader then disconnect it
+                    if( mReader != null )
+                    {
+                        if( action == DeviceListActivity.DEVICE_CHANGE || action == DeviceListActivity.DEVICE_DISCONNECT)
+                        {
+                            mReader.disconnect();
+                            if(action == DeviceListActivity.DEVICE_DISCONNECT)
+                            {
+                                mReader = null;
+                            }
+                        }
+                    }
+
+                    // Use the Reader found
+                    if( action == DeviceListActivity.DEVICE_CHANGE || action == DeviceListActivity.DEVICE_CONNECT)
+                    {
+                        mReader = chosenReader;
+                        getCommander().setReader(mReader);
+                    }
+
+                    final PluginResult result = new PluginResult(PluginResult.Status.OK, "Connected");
+                    callback.sendPluginResult(result);
+                    return;
+                }
+                break;
+        }
+        final PluginResult result = new PluginResult(PluginResult.Status.OK, "Failed");
+        callback.sendPluginResult(result);
     }
 }
--- www/rfid_asciiprotocol.js
+++ www/rfid_asciiprotocol.js
@@ -3,14 +3,20 @@ var exec = require('cordova/exec');
 var PLUGIN_NAME = 'RFIDAsciiProtocol';
 
 var RFIDAsciiProtocol = {
+    init: function(cb) {
+        exec(null, null, PLUGIN_NAME, 'init', []);
+    },
     isConnected: function(cb) {
         exec(cb, null, PLUGIN_NAME, 'isConnected', []);
     },
     connect: function(cb) {
         exec(cb, null, PLUGIN_NAME, 'connect', []);
     },
+    disconnect: function(cb) {
+        exec(null, null, PLUGIN_NAME, 'disconnect', []);
+    },
     scan: function(cb) {
-        exec(cb, null, PLUGIN_NAME, 'scan', []);
+        exec(null, null, PLUGIN_NAME, 'scan', []);
     }
 };
 module.exports = RFIDAsciiProtocol;
